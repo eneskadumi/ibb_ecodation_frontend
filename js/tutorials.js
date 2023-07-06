14f78907de3689c82b88f817fa9b22f1bcb4c9a8
eneskadumi <eneskadumi@gmail.com>
2023-07-06 16:11:39 +0300

    Javascript

---
+++ js/tutorials.js
@@ -0,0 +1,15 @@
+//single comment
+
+/* 
+multiple comment
+*/ 
+
+console.log("loglama");
+console.warn("warn");
+console.error("error");
+console.info("info");
+
+alert("pop-up bilgilendirme-1");
+window.alert("pop-up bilgilendirme-2");
+
+document.writeln("ekranda yazı");
