377021dee27e3b4cc5e7f0f8f13af8f5afc014c0
eneskadumi <eneskadumi@gmail.com>
2023-07-06 16:30:01 +0300

    whoisting, variable, console, alert

--- js/tutorials.js
+++ js/tutorials.js
@@ -1,15 +1,57 @@
-//single comment
+// JS 1995 yılında Brandan Eich tarafından yazılıyor.
+// Cross platform
+// OOP uygun.
 
+
+//single comment
 /* 
 multiple comment
 */ 
 
-console.log("loglama");
-console.warn("warn");
-console.error("error");
-console.info("info");
+// console.log("loglama");
+// console.warn("warn");
+// console.error("error");
+// console.info("info");
 
-alert("pop-up bilgilendirme-1");
-window.alert("pop-up bilgilendirme-2");
+// alert("pop-up bilgilendirme-1");
+// window.alert("pop-up bilgilendirme-2");
 
-document.writeln("ekranda yazı");
+// document.writeln("ekranda yazı");
+
+
+//Variable
+
+var result = "Js öğreniyorum";
+console.log(typeof result + " => "+ result);
+
+var result = 4423;
+console.log(typeof result + " => "+ result);
+
+var result = 44.23;
+console.log(typeof result + " => "+ result);
+
+var result = true;
+console.log(typeof result + " => "+ result);
+
+
+//Naming Convensition
+
+var deneme = "JS Data"
+console.log(deneme);
+
+var deneme44 = "JS Data"
+console.log(deneme44);
+
+var $deneme44 = "JS Data"
+console.log($deneme44);
+
+var denemeVerisi = "JS Data"
+console.log(denemeVerisi);
+
+// whoisting
+result2= 56;
+var result2;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Operators
